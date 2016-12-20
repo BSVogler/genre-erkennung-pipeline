e1a1a1d7a378ab9fd4783483e1c3ad628a0f9a3b
BSVogler <e-mail@benediktvogler.de>
2016-12-20 17:04:26 +0100

    returns "pending" if file is processed

--- cgi-bin/webAPI.py
+++ cgi-bin/webAPI.py
@@ -20,26 +20,36 @@ if len(arguments) >= 1:
     
     youtubeURL = str(arguments["youtubeurl"].value)
     rootPath = "./"
-    if not os.path.exists(rootPath+"query"):
-        os.makedirs(rootPath+"query")
-    #download file
-    print("Downloading "+youtubeURL+"<br>")
-    #cmd = "youtube-dl --extract-audio --audio-format mp3 -o "+rootPath+"query/testfile.%(ext)s "+youtubeID
-    #subprocess.call(cmd.split())
-    print("Downloaded")
-        #print("\nFile $1 downloaded \n")
-        #extract features
-        
-    youtubeID=youtubeURL[youtubeURL.find("=")+1:]
-    print(youtubeID)
+    queryPath = rootPath+"query"
+    if not os.path.exists(queryPath):
+        os.makedirs(queryPath)
     
-    import querying_genre as qg
-    qg.query(youtubeID)
-    #cmd = "python3 ./querying_genre.py query/testfile.mp3 $(cut -d "=" -f 2 <<< "+youtubeID+") #decode features"
-    #subprocess.call(cmd.split())
-    #remove -rf "query"
-   # p = Popen(cmd.split(), stdin=PIPE, stdout=PIPE, stderr=PIPE)
-    #output, err = p.communicate()
-    print("Request accepted: "+youtubeID)
+    youtubeID = youtubeURL[youtubeURL.find("=")+1:]
+    #print(youtubeID)
+    
+    #mark as waiting
+    if not os.path.exists(rootPath+"results/"+youtubeID+".txt"):
+        with open('results/'+youtubeID+".txt",'w') as resultfile:
+            resultfile.write("pending")
+            
+    #download file
+        if not os.path.exists(queryPath+"/"+youtubeID+".mp3"):    
+            print("Downloading "+youtubeURL+"<br>")
+            cmd = "youtube-dl --extract-audio --audio-format mp3 -o "+queryPath+"/"+youtubeID+".%(ext)s "+youtubeID
+            subprocess.call(cmd.split())
+            print("Downloaded")
+    
+        #extract features and query
+        import querying_genre as qg
+        qg.query(youtubeID)
+
+        #remove -rf "query"
+       # p = Popen(cmd.split(), stdin=PIPE, stdout=PIPE, stderr=PIPE)
+        #output, err = p.communicate()
+        print("Request accepted: "+youtubeID+"<br>")
+    else:
+        with open('results/'+youtubeID+".txt",'r') as resultfile:
+            print(resultfile.read())
+            
 else:
     print("missing or empty argument <i>youtubeurl</i>")
