8fc8d2431fdcdc9f80cdfb55ac4e2fdb97bc2733
BSVogler <mail@benediktsvogler.com>
2018-10-01 14:18:09 +0200

    format

--- cgi-bin/merged.py
+++ cgi-bin/merged.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 import numpy as np
+
 np.random.seed(1337)  # for reproducibility
 from keras.models import Sequential
 from keras.layers import Dense, Dropout, Activation, Merge
@@ -19,22 +20,22 @@ nb_epoch = 300
 numGenres = 3
 
 if not os.path.exists("model_weights"):
-   os.makedirs("model_weights")
-  
-#check if can write to file
+    os.makedirs("model_weights")
+
+# check if can write to file
 f = open('model_weights/mfcc_model_weights.hdf5', 'a')
 f.write('test')
 f.close()
-    
+
 print("creating model")
 # create model
 datasetfolder = "../pickled_vectors"
 
-X_1 = pickle.load(open(datasetfolder+"/mfcc_coefficients_training_vector.pickle","rb"))
-X_test_1 = pickle.load(open(datasetfolder+"/mfcc_coefficients_evaluation_training_vector.pickle","rb"))
+X_1 = pickle.load(open(datasetfolder + "/mfcc_coefficients_training_vector.pickle", "rb"))
+X_test_1 = pickle.load(open(datasetfolder + "/mfcc_coefficients_evaluation_training_vector.pickle", "rb"))
 
-X_2 = pickle.load(open(datasetfolder+"/spectral-contrast_peaks_training_vector.pickle","rb"))
-X_test_2 = pickle.load(open(datasetfolder+"/spectral-contrast_peaks_evaluation_training_vector.pickle","rb"))
+X_2 = pickle.load(open(datasetfolder + "/spectral-contrast_peaks_training_vector.pickle", "rb"))
+X_test_2 = pickle.load(open(datasetfolder + "/spectral-contrast_peaks_evaluation_training_vector.pickle", "rb"))
 
 model_1 = mfcc_model.mfcc_model((X_1.shape[1], X_1.shape[2]), True)
 model_2 = spectral_contrast_peaks_model.model((X_2.shape[1], X_2.shape[2]), True)
@@ -51,9 +52,9 @@ final_model.add(Merge([model_1, model_2], mode="concat"))
 final_model.add(Dense(100))
 final_model.add(Dense(numGenres, activation='softmax'))
 final_model.compile(
-              loss='categorical_crossentropy',
-              optimizer='adam',
-              metrics=['accuracy']
+    loss='categorical_crossentropy',
+    optimizer='adam',
+    metrics=['accuracy']
 )
 
 # plot(model_1,to_file="model_1.png")
@@ -61,8 +62,8 @@ final_model.compile(
 # plot(final_model,to_file="merged_model.png")
 
 json_string = final_model.to_json()
-with open("../model_architecture/merged_model_architecture.json","w") as f:
-    f.write(json.dumps(json_string, sort_keys=True,indent=4, separators=(',', ': ')))
+with open("../model_architecture/merged_model_architecture.json", "w") as f:
+    f.write(json.dumps(json_string, sort_keys=True, indent=4, separators=(',', ': ')))
 
 print("Fitting")
 # #
@@ -71,40 +72,39 @@ print("Fitting")
 # # for i in range(10):
 # #     print("epoch",i)
 
-y = pickle.load(open(datasetfolder+"/mfcc_coefficients_label.pickle","rb"))
-y_test = pickle.load(open(datasetfolder+"/mfcc_coefficients_evaluation_label.pickle","rb"))
+y = pickle.load(open(datasetfolder + "/mfcc_coefficients_label.pickle", "rb"))
+y_test = pickle.load(open(datasetfolder + "/mfcc_coefficients_evaluation_label.pickle", "rb"))
 print("y", y.shape)
 print("y_test", y_test.shape)
 
 if not os.path.exists("model_weights"):
     os.makedirs("model_weights")
-    
+
 # checkpoint
-filepath="weights-improvement-{epoch:02d}-{val_acc:.2f}.hdf5"
+filepath = "weights-improvement-{epoch:02d}-{val_acc:.2f}.hdf5"
 checkpoint = ModelCheckpoint(filepath, monitor='val_acc', verbose=1, save_best_only=True, mode='max')
 callbacks_list = [checkpoint]
 
-history = final_model.fit([X_1,X_2], y,
-                            batch_size=batch_size,
-                            nb_epoch=nb_epoch,
-                            validation_data=([X_test_1, X_test_2], y_test),
-                            shuffle="batch",
-                            callbacks=callbacks_list, 
-                            )
+history = final_model.fit([X_1, X_2], y,
+                          batch_size=batch_size,
+                          nb_epoch=nb_epoch,
+                          validation_data=([X_test_1, X_test_2], y_test),
+                          shuffle="batch",
+                          callbacks=callbacks_list,
+                          )
 print("saving final result")
-final_model.save_weights("model_weights/merged_model_weights.hdf5",overwrite=True)
+final_model.save_weights("model_weights/merged_model_weights.hdf5", overwrite=True)
 
-with open("experimental_results.json","w") as f:
-    f.write(json.dumps(history.history, sort_keys=True,indent=4, separators=(',', ': ')))
+with open("experimental_results.json", "w") as f:
+    f.write(json.dumps(history.history, sort_keys=True, indent=4, separators=(',', ': ')))
 
-
-for k,v in history.history.items():
+for k, v in history.history.items():
     _keys = list(history.history.keys())
     _keys.sort()
-    plt.subplot(411+_keys.index(k))
+    plt.subplot(411 + _keys.index(k))
     plt.title(k)
 
-    plt.plot(range(0,len(v)),v,marker="8",linewidth=1.5)
-    
+    plt.plot(range(0, len(v)), v, marker="8", linewidth=1.5)
+
 plt.tight_layout(pad=0.4, w_pad=0.5, h_pad=1.0)
 plt.savefig('history.png')
