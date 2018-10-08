2efa00154b3298a325691fef3fe74e613bf77fdb
BSVogler <mail@benediktsvogler.com>
2018-10-08 19:40:02 +0200

    format

--- cgi-bin/spectral_contrast_peaks_model.py
+++ cgi-bin/spectral_contrast_peaks_model.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 import numpy as np
+
 np.random.seed(1337)  # for reproducibility
 from keras.models import Sequential
 from keras.layers import Dense, Dropout, Activation
@@ -9,17 +10,16 @@ import matplotlib as mpl
 import matplotlib.pyplot as plt
 import pickle
 
-numGenres=3
-    
+numGenres = 3
+
+
 # load vectorized song features
 #
 def model(input_shape, concat):
-
     nb_filter = 100
     filter_length = 3
     hidden_dims = 250
     pool_length = 1
-
 
     # LSTM
     lstm_output_size = 100
@@ -29,24 +29,23 @@ def model(input_shape, concat):
     model = Sequential()
     #
     model.add(Convolution1D(
-                            input_shape=input_shape,
-                            nb_filter=nb_filter,
-                            filter_length=filter_length,
-                            border_mode='valid',
-                            subsample_length=4))
+        input_shape=input_shape,
+        nb_filter=nb_filter,
+        filter_length=filter_length,
+        border_mode='valid',
+        subsample_length=4))
     model.add(Activation('relu'))
     model.add(MaxPooling1D(pool_length=pool_length))
     model.add(Dropout(0.2))
 
     model.add(LSTM(lstm_output_size,
-                    # input_shape=input_shape,
-                    activation='sigmoid',
-                    inner_activation='hard_sigmoid',
-                    # return_sequences=True
-                    ))
+                   # input_shape=input_shape,
+                   activation='sigmoid',
+                   inner_activation='hard_sigmoid',
+                   # return_sequences=True
+                   ))
 
     model.add(Dropout(0.2))
-
 
     #
     #
@@ -94,6 +93,7 @@ def model(input_shape, concat):
     # model.add(LSTM(lstm_output_size))
     return model
 
+
 # model.add(LSTM(lstm_output_size,
 #                 # input_shape=(X.shape[1],X.shape[2]),
 #                 activation='sigmoid',
@@ -124,7 +124,7 @@ def model(input_shape, concat):
 # model.add(Activation("sigmoid"))
 # model.add(Dropout(0.2))
 
-if __name__=="__main__":
+if __name__ == "__main__":
     datasetfolder = "../pickled_vectors"
     X = pickle.load(open(datasetfolder + "/spectral-contrast_peaks_training_vector.pickle", "rb"))
     y = pickle.load(open(datasetfolder + "/spectral-contrast_peaks_label.pickle", "rb"))
@@ -134,7 +134,7 @@ if __name__=="__main__":
 
     batch_size = 20
     nb_epoch = 50
-    model = model((X.shape[1],X.shape[2]))
+    model = model((X.shape[1], X.shape[2]))
     model.add(Dense(numGenres))
     model.add(Activation('softmax'))
 
@@ -143,42 +143,41 @@ if __name__=="__main__":
                   metrics=['accuracy']
                   )
 
-
-
     # print(X)
-    print("X shape",X.shape)
-    print("y shape",y.shape)
+    print("X shape", X.shape)
+    print("y shape", y.shape)
     print("X_test", X_test.shape)
     print("y_test", y_test.shape)
 
-    print("Fitting")
+    print("Training")
     #
     # X = np.random.random(X.shape)
     # y = np.random.random(y.shape)
     #
     history = model.fit(X, y,
-              batch_size=batch_size,
-              nb_epoch=nb_epoch,
-              validation_data=(X_test, y_test),
-              shuffle=True
-            )
+                        batch_size=batch_size,
+                        nb_epoch=nb_epoch,
+                        validation_data=(X_test, y_test),
+                        shuffle=True
+                        )
     #
     # # with open("experimental_results.json","w") as f:
     # #     f.write(json.dumps(history.history, sort_keys=True,indent=4, separators=(',', ': ')))
     #
     import os
+
     if not os.path.exists("model_weights"):
         os.makedirs("model_weights")
-    model.save_weights("model_weights/spectral_contrast_peaks_model_weights.hdf5",overwrite=True)    
-    for k,v in history.history.items():
+    model.save_weights("model_weights/spectral_contrast_peaks_model_weights.hdf5", overwrite=True)
+    for k, v in history.history.items():
         # print(k,v)
         _keys = list(history.history.keys())
         _keys.sort()
-        plt.subplot(411+_keys.index(k))
+        plt.subplot(411 + _keys.index(k))
         # x_space = np.linspace(0,1,len(v))
         plt.title(k)
 
-        plt.plot(range(0,len(v)),v,marker="8",linewidth=1.5)
+        plt.plot(range(0, len(v)), v, marker="8", linewidth=1.5)
 
     plt.tight_layout(pad=0.4, w_pad=0.5, h_pad=1.0)
     plt.savefig('history.png')
