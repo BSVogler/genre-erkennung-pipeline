df9536bedf78fb1b32b23db7c8253a229e74aee1
BSVogler <mail@benediktsvogler.com>
2018-10-08 19:42:29 +0200

    fomratting

--- cgi-bin/mfcc_model.py
+++ cgi-bin/mfcc_model.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import numpy as np
+
 np.random.seed(1337)  # for reproducibility
 from keras.models import Sequential
 from keras.layers import Dense, Dropout, Activation
@@ -11,7 +12,9 @@ import matplotlib.pyplot as plt
 import pickle
 import json
 
-numGenres=3
+numGenres = 3
+
+
 def mfcc_model(input_shape, concat):
     nb_filter = 100
     filter_length = 4
@@ -26,27 +29,23 @@ def mfcc_model(input_shape, concat):
     model = Sequential()
 
     model.add(Convolution1D(
-                            input_shape=input_shape,
-                            nb_filter=nb_filter,
-                            filter_length=filter_length,
-                            border_mode='valid',
-                            subsample_length=1))
+        input_shape=input_shape,
+        nb_filter=nb_filter,
+        filter_length=filter_length,
+        border_mode='valid',
+        subsample_length=1))
     model.add(Activation('relu'))
     model.add(MaxPooling1D(pool_length=pool_length))
     model.add(Dropout(0.4))
-    # #
-    # # #
     model.add(Convolution1D(
-                            nb_filter=int(nb_filter/5),
-                            filter_length=int(filter_length),
-                            border_mode='valid',
-                            subsample_length=1))
+        nb_filter=int(nb_filter / 5),
+        filter_length=int(filter_length),
+        border_mode='valid',
+        subsample_length=1))
     model.add(Activation('relu'))
     model.add(MaxPooling1D(pool_length=pool_length))
     model.add(Dropout(0.4))
     # model.add(Flatten())
-    # # #
-    # # #
     #
     # model.add(Convolution1D(
     #                         nb_filter=int(nb_filter/10),
@@ -58,13 +57,11 @@ def mfcc_model(input_shape, concat):
     # model.add(Dropout(0.2))
     #
     model.add(LSTM(lstm_output_size,
-                    # input_shape=input_shape,
-                    activation='sigmoid',
-                    inner_activation='hard_sigmoid'))
+                   # input_shape=input_shape,
+                   activation='sigmoid',
+                   inner_activation='hard_sigmoid'))
     #
     # model.add(Dropout(0.2))
-
-
 
     # model.add(Flatten())
     # model.add(LSTM(lstm_output_size))
@@ -82,7 +79,6 @@ def mfcc_model(input_shape, concat):
     # model.add(MaxPooling1D(pool_length=pool_length))
     # model.add(Dropout(0.4))
 
-
     # model.add(Lambda(max_1d, output_shape=(nb_filter)))
     # model.add(LSTM(lstm_output_size))
     # model.add(Dropout(0.2))
@@ -96,28 +92,30 @@ def mfcc_model(input_shape, concat):
 
     return model
 
-if __name__=="__main__":
+
+if __name__ == "__main__":
     import os
+
     if not os.path.exists("model_weights"):
         os.makedirs("model_weights")
-        
-    #check if can write to file
+
+    # check if can write to file
     f = open('model_weights/mfcc_model_weights.hdf5', 'a')
     f.write('test')
     f.close()
-   
+
     # print(X)
     # load vectorized song features
     #
     batch_size = 20
     nb_epoch = 50
-    X = pickle.load(open("pickled_vectors/mfcc_coefficients_training_vector.pickle","rb"))
-    y = pickle.load(open("pickled_vectors/mfcc_coefficients_label.pickle","rb"))
+    X = pickle.load(open("pickled_vectors/mfcc_coefficients_training_vector.pickle", "rb"))
+    y = pickle.load(open("pickled_vectors/mfcc_coefficients_label.pickle", "rb"))
 
-    X_test = pickle.load(open("pickled_vectors/mfcc_coefficients_evaluation_training_vector.pickle","rb"))
-    y_test = pickle.load(open("pickled_vectors/mfcc_coefficients_evaluation_label.pickle","rb"))
+    X_test = pickle.load(open("pickled_vectors/mfcc_coefficients_evaluation_training_vector.pickle", "rb"))
+    y_test = pickle.load(open("pickled_vectors/mfcc_coefficients_evaluation_label.pickle", "rb"))
 
-    model = mfcc_model((X.shape[1],X.shape[2]))
+    model = mfcc_model((X.shape[1], X.shape[2]))
     model.add(Dense(numGenres))
     model.add(Activation('softmax'))
     model.compile(loss='categorical_crossentropy',
@@ -125,34 +123,34 @@ if __name__=="__main__":
                   metrics=['accuracy']
                   )
 
-    print("X shape",X.shape)
-    print("y shape",y.shape)
+    print("X shape", X.shape)
+    print("y shape", y.shape)
     print("X_test", X_test.shape)
     print("y_test", y_test.shape)
 
     print("Fitting")
 
     history = model.fit(X, y,
-              batch_size=batch_size,
-              nb_epoch=nb_epoch,
-              validation_data=(X_test, y_test),
-              shuffle="batch"
-            )
+                        batch_size=batch_size,
+                        nb_epoch=nb_epoch,
+                        validation_data=(X_test, y_test),
+                        shuffle="batch"
+                        )
 
-    with open("experimental_results.json","w") as f:
+    with open("experimental_results.json", "w") as f:
         f.write(json.dumps(history.history, sort_keys=True, indent=4, separators=(',', ': ')))
 
-    for k,v in history.history.items():
+    for k, v in history.history.items():
         # print(k,v)
         _keys = list(history.history.keys())
         _keys.sort()
-        plt.subplot(411+_keys.index(k))
+        plt.subplot(411 + _keys.index(k))
         # x_space = np.linspace(0,1,len(v))
         plt.title(k)
 
-        plt.plot(range(0,len(v)),v,marker="8",linewidth=1.5)
-        
-    model.save_weights("model_weights/mfcc_model_weights.hdf5",overwrite=True)
-    
+        plt.plot(range(0, len(v)), v, marker="8", linewidth=1.5)
+
+    model.save_weights("model_weights/mfcc_model_weights.hdf5", overwrite=True)
+
     plt.tight_layout(pad=0.4, w_pad=0.5, h_pad=1.0)
     plt.savefig('history.png')
