597815c13445bc4b4fcb05e386b3a4aed5fab327
BSVogler <developer@benediktsvogler.com>
2020-09-03 14:26:39 +0200

    fix youtube-dl install script

--- setup/install-youtubedl.sh
+++ setup/install-youtubedl.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-sudo curl https://yt-dl.org/latest/youtube-dl -o /usr/local/bin/youtube-dl
+sudo curl -L https://yt-dl.org/downloads/latest/youtube-dl -o /usr/local/bin/youtube-dl
 sudo chmod a+rx /usr/local/bin/youtube-dl
