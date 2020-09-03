56942024d77fd3eba489c4a9bac4eea324127674
BSVogler <developer@benediktsvogler.com>
2020-09-03 15:43:33 +0200

    remove unused code

--- cgi-bin/extract_features.py
+++ cgi-bin/extract_features.py
@@ -11,7 +11,8 @@ audio_features = [
                       "vamp:bbc-vamp-plugins:bbc-spectral-contrast:peaks",
                       "vamp:bbc-vamp-plugins:bbc-spectral-contrast:valleys",
                       ]
-                      
+
+
 def extract_features(path="."):
     for feature in audio_features:
         cmd = "sonic-annotator", "-d", feature, path, "-r", "-w", "csv", "--csv-force"
@@ -19,28 +20,21 @@ def extract_features(path="."):
         #p = Popen(cmd.split(), stdin=PIPE, stdout=PIPE, stderr=PIPE)
         #output, err = p.communicate()
 
+
 def extract_features_single(path="."):
     for feature in audio_features:
         cmd = "sonic-annotator", "-d", feature, path, "-w", "csv", "--csv-force"
         subprocess.call(cmd)
 
+
 if __name__=="__main__":
-    # extract_features("dataset/gztan_split_10sec")
-
-    #    extract_features_single(audio_path)
-    # extract_features("dataset/train/hiphop")
-    # extract_features("dataset/train/rock")
-    # extract_features("dataset/train/pop")
-
-    # extract_features("dataset/train")
     if len(sys.argv) < 2:
         print("missing parameter for dataset or file path")
     else:
         if os.path.isdir(sys.argv[1]):
             for file in iterate_audio(path=sys.argv[1]):
-                print("extracing" + file)
+                print("extracting " + file)
                 extract_features_single(file)
         else:
+            # extract features for single file
             extract_features_single(sys.argv[1])
-    
-    #extract features for single file
